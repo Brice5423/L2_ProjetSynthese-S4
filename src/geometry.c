2ba950c3ee8b2cebbc1e3fadbe3f29c1a060b641
Matthieu <mattfritsch98380@gmail.com>
2022-04-12 17:44:32 +0200

    🟠 ~ geometry.c (tout)

--- src/geometry.c
+++ src/geometry.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "../include/geometry.h"
 #include "../include/util.h"
 
+
 Point *newPoint(long long int x, long long int y) {
-    // TODO
+    Point *Point;
+    if(Point == NULL)
+        ShowMessage("Le point Point est null", 1);
+    Point = calloc(1, sizeof(Point));
+    assert(Point);
+    Point->x = x;
+    Point->y = y;
+
+    return Point;
 }
 
 long long int X(const Point *P) {
@@ -16,45 +26,83 @@ long long int Y(const Point *P) {
 }
 
 void viewPoint(const void *P) {
-    //TODO
+    printf("\nPoint ( Abscisse : %lld ; Ordonnée : %lld )\n", X((Point *)P), Y((Point *)P));
 }
 
 void freePoint(void *P) {
     free(P);
 }
-
+/* < 0 : à droite
+ * > 0 : à gauche
+ * = 0 : colinéaire
+ * A : origin
+ * B: destination
+ */
 int onRight(const Point *origin, const Point *destination, const Point *P) {
-    // TODO
+    if( (X(destination) - X(origin)) * (Y(P) - Y(origin)) - (Y(destination) - Y(origin)) * (X(P) - X(origin)) < 0)
+        return 1;
+    return 0;
 }
 
 int onLeft(const Point *origin, const Point *destination, const Point *P) {
-    // TODO
+    if( (X(destination) - X(origin)) * (Y(P) - Y(origin)) - (Y(destination) - Y(origin)) * (X(P) - X(origin)) > 0)
+        return 1;
+    return 0;
 }
 
 int collinear(const Point *origin, const Point *destination, const Point *P) {
-    // TODO
+    // TODO : à tester collinear
+    if( (X(destination) - X(origin)) * (Y(P) - Y(origin)) - (Y(destination) - Y(origin)) * (X(P) - X(origin)) == 0)
+        return 1;
+    return 0;
 }
 
 int isIncluded(const Point *origin, const Point *destination, const Point *P) {
-    // TODO
+    //verifier si P est colinéaire
+    if(collinear(origin, destination, P)){
+        //si colinéaire alors vérifier que l'abscisse de P est inférieur à l'abscisse de destination et superieur à l'abscisse de l'origine
+        if((X(P) < X(destination)) && (X(P) > X(origin))){
+            //si c'est vrai alors faire pareil pour l'ordonnée
+            if((Y(P) < Y(destination)) && (Y(P) > Y(origin))){
+                //si tout est vrai alors on retourne 1
+                return 1;
+            }
+            return 0;
+        }
+        return 0;
+    }
+    return 0;
 }
 
 DEdge *newDEdge(Point *origin, Point *destination) {
-    // TODO
+
+    DEdge *DE;
+
+    DE = calloc(1, sizeof(DEdge));
+    assert(DE);
+    DE->origin = origin;
+    DE->destination = destination;
+
+    return DE;
 }
 
 Point *getOrigin(const DEdge *DE) {
-    // TODO
+    return DE->origin;
 }
 
 Point *getDestination(const DEdge *DE) {
-    // TODO
+    return DE->destination;
 }
 
 void viewDEdge(const void *DE) {
-    // TODO
+    printf("\nOrigine de l'arc : \n");
+    viewPoint(getOrigin(DE));
+    printf("\nDestination de l'arc : ");
+    viewPoint(getDestination(DE));
 }
 
 void freeDEdge(void *DE) {
-    // TODO
+    free(getOrigin(DE));
+    free(getDestination(DE));
+    free(DE);
 }
