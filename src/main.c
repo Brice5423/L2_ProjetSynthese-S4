e238438fddbaa0df7b1fcbdb01fb932937f68b21
BriceBriçou <briceorl54580@gmail.com>
2022-04-29 18:55:05 +0200

    🟩 + main.c (ajout du a pour all)

--- src/main.c
+++ src/main.c
@@ -12,31 +12,62 @@
 int main(int argc, char *argv[]) {
     const char algo = argv[1][0];
 
-    if (argc == 4) {
-        if (algo == 's') {
-            SlowConvexHull(argv[2], argv[3]);
-            printf("\nOpération bien réussie.\n");
+    if (algo != 'a') {
+        if (argc == 4) {
+            if (algo == 's') {
+                SlowConvexHull(argv[2], argv[3]);
+                printf("\nOpération bien réussie.\n");
+
+            } else if (algo == 'r') {
+                RapidConvexHull(argv[2], argv[3]);
+                printf("\nOpération bien réussie.\n");
+
+            } else if (algo == 'c') {
+                printf("\nUsage : \n\t ./convexhull c infilename outfilename sort\n\t\tsort :\n\t\t\t 1 (CBTHeapSort)\n\t\t\t 2 (ArrayHeapSort)\n\t\t\t 3 (SelectionSort)\n");
+            }
+
+        } else if (argc == 5) {
+            if (algo == 's') {
+                printf("\nUsage : \n\t ./convexhull s infilename outfilename\n");
+
+            } else if (algo == 'r') {
+                printf("\nUsage : \n\t ./convexhull r infilename outfilename\n");
+
+            } else if (algo == 'c') {
+                ConvexHull(argv[2], argv[3], atoi(argv[4]));
+                printf("\nOpération bien réussie.\n");
+            }
+
+        } else {
+            printf("\nUsage : \n\t ./convexhull algo infilename outfilename sort\n\talgo est égale à s (SlowConvexHull) ou c (ConvexHull) ou r (RapidConvexHull)\n\tsort est égale à 1 ou 2 ou 3\n");
         }
-        if (algo == 'r') {
-            RapidConvexHull(argv[2], argv[3]);
-            printf("\nOpération bien réussie.\n");
-        }
-        if (algo == 'c') {
-            printf("\nUsage : \n\t ./convexhull c infilename outfilename sort\n\t\tsort :\n\t\t\t 1 (CBTHeapSort)\n\t\t\t 2 (ArrayHeapSort)\n\t\t\t 3 (SelectionSort)\n");
-        }
-    } else if (argc == 5) {
-        if (algo == 's') {
-            printf("\nUsage : \n\t ./convexhull s infilename outfilename\n");
-        }
-        if (algo == 'r') {
-            printf("\nUsage : \n\t ./convexhull r infilename outfilename\n");
-        }
-        if (algo == 'c') {
-            ConvexHull(argv[2], argv[3], atoi(argv[4]));
-            printf("\nOpération bien réussie.\n");
-        }
+
     } else {
-        printf("\nUsage : \n\t ./convexhull algo infilename outfilename sort\n\talgo est égale à s (SlowConvexHull) ou c (ConvexHull) ou r (RapidConvexHull)\n\tsort est égale à 1 ou 2 ou 3\n");
+        RapidConvexHull("data/data1", "batterieTest/convex/rapidConvex_Data1");
+        RapidConvexHull("data/data2", "batterieTest/convex/rapidConvex_Data2");
+        RapidConvexHull("data/data3", "batterieTest/convex/rapidConvex_Data3");
+        RapidConvexHull("data/data4", "batterieTest/convex/rapidConvex_Data4");
+
+        ConvexHull("data/data1", "batterieTest/convex/convex_Data1_SortBy1", 1);
+        ConvexHull("data/data1", "batterieTest/convex/convex_Data1_SortBy2", 2);
+        ConvexHull("data/data1", "batterieTest/convex/convex_Data1_SortBy3", 3);
+
+        ConvexHull("data/data2", "batterieTest/convex/convex_Data2_SortBy1", 1);
+        ConvexHull("data/data2", "batterieTest/convex/convex_Data2_SortBy2", 2);
+        ConvexHull("data/data2", "batterieTest/convex/convex_Data2_SortBy3", 3);
+
+        ConvexHull("data/data3", "batterieTest/convex/convex_Data3_SortBy1", 1);
+        ConvexHull("data/data3", "batterieTest/convex/convex_Data3_SortBy2", 2);
+        ConvexHull("data/data3", "batterieTest/convex/convex_Data3_SortBy3", 3);
+
+        ConvexHull("data/data4", "batterieTest/convex/convex_Data4_SortBy1", 1);
+        ConvexHull("data/data4", "batterieTest/convex/convex_Data4_SortBy2", 2);
+        ConvexHull("data/data4", "batterieTest/convex/convex_Data4_SortBy3", 3);
+
+        SlowConvexHull("data/data1", "batterieTest/convex/slowConvex_Data1");
+        SlowConvexHull("data/data2", "batterieTest/convex/slowConvex_Data2");
+        SlowConvexHull("data/data3", "batterieTest/convex/slowConvex_Data3");
+        SlowConvexHull("data/data4", "batterieTest/convex/slowConvex_Data4");
     }
 
 
