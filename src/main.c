992769de31ed7b556be20bee59c9fa8ce80d0ff5
Matthieu <mattfritsch98380@gmail.com>
2022-04-29 18:30:18 +0200

    🟧 ~ main.c

--- src/main.c
+++ src/main.c
@@ -10,24 +10,34 @@
 #include "../include/algo.h"
 
 int main(int argc, char *argv[]) {
-    printf("Il y a %d argument et argv[0] : %s\n\n", argc, argv[0]);
+    const char algo = argv[1][0];
 
-    ConvexHull("data/data1", "batterieTest/convex/convex_Data1_SortBy1", 1);
-    ConvexHull("data/data1", "batterieTest/convex/convex_Data1_SortBy2", 2);
-    ConvexHull("data/data1", "batterieTest/convex/convex_Data1_SortBy3", 3);
-
-    ConvexHull("data/data2", "batterieTest/convex/convex_Data2_SortBy1", 1);
-    ConvexHull("data/data2", "batterieTest/convex/convex_Data2_SortBy2", 2);
-    ConvexHull("data/data2", "batterieTest/convex/convex_Data2_SortBy3", 3);
-
-    ConvexHull("data/data3", "batterieTest/convex/convex_Data3_SortBy1", 1);
-    ConvexHull("data/data3", "batterieTest/convex/convex_Data3_SortBy2", 2);
-    ConvexHull("data/data3", "batterieTest/convex/convex_Data3_SortBy3", 3);
-
-    ConvexHull("data/data4", "batterieTest/convex/convex_Data4_SortBy1", 1);
-    ConvexHull("data/data4", "batterieTest/convex/convex_Data4_SortBy2", 2);
-    ConvexHull("data/data4", "batterieTest/convex/convex_Data4_SortBy3", 3);
-
+    if (argc == 4) {
+        if (algo == 's') {
+            SlowConvexHull(argv[2], argv[3]);
+            printf("\nOpération bien réussie.\n");
+        }
+        if (algo == 'r') {
+            RapidConvexHull(argv[2], argv[3]);
+            printf("\nOpération bien réussie.\n");
+        }
+        if (algo == 'c') {
+            printf("\nUsage : \n\t ./convexhull c infilename outfilename sort\n\t\tsort :\n\t\t\t 1 (CBTHeapSort)\n\t\t\t 2 (ArrayHeapSort)\n\t\t\t 3 (SelectionSort)\n");
+        }
+    } else if (argc == 5) {
+        if (algo == 's') {
+            printf("\nUsage : \n\t ./convexhull s infilename outfilename\n");
+        }
+        if (algo == 'r') {
+            printf("\nUsage : \n\t ./convexhull r infilename outfilename\n");
+        }
+        if (algo == 'c') {
+            ConvexHull(argv[2], argv[3], atoi(argv[4]));
+            printf("\nOpération bien réussie.\n");
+        }
+    } else {
+        printf("\nUsage : \n\t ./convexhull algo infilename outfilename sort\n\talgo est égale à s (SlowConvexHull) ou c (ConvexHull) ou r (RapidConvexHull)\n\tsort est égale à 1 ou 2 ou 3\n");
+    }
 
 
     return EXIT_SUCCESS;
